bd95c9b1909bd40bd6a6c4868c5192c4fd5734a9
Kelby Mittan <kelbymittan@pursuit.org>
2020-12-22 12:49:08 -0500

    paul hudson starter

---
+++ Trekr/Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Kelby Mittan on 12/22/20.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
